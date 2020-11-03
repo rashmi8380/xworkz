1129c0ddcc29a6564399f9756af4a0a3d9ad888c
rashmi8380 <73732851+rashmi8380@users.noreply.github.com>
2020-11-03 18:21:33 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd{
+	public static void main(String args[]){
+	int a[]={6,5,4,3,2,1};
+	for(int i=0;i<5;i++){
+		if(a[i]%2==0)
+		{	
+			System.out.println(a[i]+ "is a even number");
+		}
+		else
+		{
+			System.out.println(a[i]+"is a odd number");
+		}
+	}
+	}
+}
