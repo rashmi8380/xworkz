4d0d04688f664ec685e9bad057dac3c1f8464e89
rashmi8380 <73732851+rashmi8380@users.noreply.github.com>
2020-11-03 18:21:04 +0530

    Add files via upload

---
+++ EvenIndex.java
@@ -0,0 +1,10 @@
+class EvenIndex{
+	public static void main(String args[]){
+	int num[]={2,3,4,8,10,12,11,13};
+		for(int i=0;i<7;i++){
+			if(num[i]%2==0){
+				System.out.println(num[i]);
+				}
+			}
+		}
+}
