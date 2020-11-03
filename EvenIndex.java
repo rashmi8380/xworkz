cf8bbbed3d4a30261b3dedaad8114e6830ea0e39
rashmi8380 <73732851+rashmi8380@users.noreply.github.com>
2020-11-03 18:26:40 +0530

    Update EvenIndex.java

--- EvenIndex.java
+++ EvenIndex.java
@@ -1,6 +1,6 @@
 class EvenIndex{
 	public static void main(String args[]){
-	int num[]={2,3,4,8,10,12,11,13};
+	int num[]={8,10,78,65,45,60,30,45};
 		for(int i=0;i<7;i++){
 			if(num[i]%2==0){
 				System.out.println(num[i]);
