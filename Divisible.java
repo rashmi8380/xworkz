2bad598a38d3b29286fbfb53ad9084087e371740
rashmi8380 <73732851+rashmi8380@users.noreply.github.com>
2020-11-03 18:25:10 +0530

    Update Divisible.java

--- Divisible.java
+++ Divisible.java
@@ -1,6 +1,6 @@
 class Divisible{
 	public static void main(String args[]){
-	int a[]={23,43,42,65,89,24};
+	int a[]={12,20,32,23,13,11};
 	for(int i=0;i<5;i++){
 	if(a[i]%2==0){
 		System.out.println(a[i]);
